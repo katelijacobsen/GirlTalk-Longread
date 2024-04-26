2986319a59b523963c1b2915d226e5a1413c28ea
Katja Mähleke <katjamaehleke98@gmail.com>
2024-04-26 18:54:49 +0200

    Add yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://katelijacobsen.github.io',
+  base: 'GirlTalk-Longread',
   integrations: [react()],
 });
